4ab3514153ec9eab745d4f62f59d8ace24b5eea3
Silvio Contreiras <silvio.contreiras@dft-sp-wkn779.dafiti.local>
2019-08-05 12:58:03 -0300

    created two simples components

--- src/Table.js
+++ src/Table.js
@@ -1,33 +1,25 @@
 import React, { Component } from 'react'
 
+
+const TableHeader =  () => (
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Job</th>
+        </tr>
+    </thead>    
+)
+
+const TableBody = () => (
+    <tbody />
+)
+
 class Table extends Component {
     render() {
         return (
             <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Job</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Chalie</td>
-                        <td>Janitor</td>
-                    </tr>
-                    <tr>
-                        <td>Mac</td>
-                        <td>Bouncer</td>
-                    </tr>
-                    <tr>
-                        <td>Dee</td>
-                        <td>Aspiring actress</td>
-                    </tr>
-                    <tr>
-                        <td>Dennis</td>
-                        <td>Bartender</td>
-                    </tr>
-                </tbody>
+                <TableHeader />
+                <TableBody />
             </table>
         )
     }
