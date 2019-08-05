ecead1498cb3f0139168bfce5beb047558b16840
SilvioContreiras <silvio7dcontreiras@gmail.com>
2019-08-04 23:46:20 -0300

    create components, App, and table

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import React, { Component } from 'react'
+import Table from './Table'
 
 class App extends Component {
     render() {
         return (
-            <div className='App'>
-                <h1>Hello, React js!</h1>
+            <div className='container'>
+                <Table />
             </div>
         )
     }
--- src/Table.js
+++ src/Table.js
@@ -1,4 +1,4 @@
-import React, { Componet } from 'react'
+import React, { Component } from 'react'
 
 class Table extends Component {
     render() {
