4315159a09a68b5e1f9eca9917e237d7862e9e64
SilvioContreiras <silvio7dcontreiras@gmail.com>
2019-08-04 23:31:54 -0300

    create component Table

---
+++ src/Table.js
@@ -0,0 +1,36 @@
+import React, { Componet } from 'react'
+
+class Table extends Component {
+    render() {
+        return (
+            <table>
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Job</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Chalie</td>
+                        <td>Janitor</td>
+                    </tr>
+                    <tr>
+                        <td>Mac</td>
+                        <td>Bouncer</td>
+                    </tr>
+                    <tr>
+                        <td>Dee</td>
+                        <td>Aspiring actress</td>
+                    </tr>
+                    <tr>
+                        <td>Dennis</td>
+                        <td>Bartender</td>
+                    </tr>
+                </tbody>
+            </table>
+        )
+    }
+}
+
+export default Table
