3556e5cad9f4d53071f7d42da48d13c20234e1ad
Silvio Contreiras <silvio.contreiras@dft-sp-wkn779.dafiti.local>
2019-08-06 10:44:44 -0300

    using props to add data to the table

--- src/App.js
+++ src/App.js
@@ -4,9 +4,32 @@ import Table from './Table'
 
 class App extends Component {
     render() {
+        const characters = [
+            {
+                name: 'Charlie',
+                job: 'Janitor',
+            },
+            {
+                name: 'Mac',
+                job: 'Bouncer',
+            },
+            {
+                name: 'Dee',
+                job: 'Aspiring actress',
+            },
+            {
+                name: 'Dennis',
+                job: 'Bartender',
+            },
+        ]
+
+
         return (
+
+             /* characterData is not a reserved keyword  is a kind of how we passa data through data- */ 
+
             <div className='container'>
-                <Table />
+                <Table characterData={characters} />
             </div>
         )
     }
--- src/Table.js
+++ src/Table.js
@@ -2,24 +2,31 @@ import React, { Component } from 'react'
 
 
 const TableHeader =  () => (
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Job</th>
-        </tr>
-    </thead>    
+    <thead />  
 )
 
-const TableBody = () => (
-    <tbody />
-)
+const TableBody = props => {
+    const rows = props.characterData.map((row, index) => {
+      return (
+        <tr key={index}>
+          <td>{row.name}</td>
+          <td>{row.job}</td>
+        </tr>
+      )
+    })
+  
+    return <tbody>{rows}</tbody>
+  }
+
 
 class Table extends Component {
     render() {
+        const { characterData } = this.props
+
         return (
             <table>
                 <TableHeader />
-                <TableBody />
+                <TableBody characterData={characterData} />
             </table>
         )
     }
